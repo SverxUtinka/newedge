484c008fafc526a888c55e5e6d369c832442a6fc
SverxUtinka <76646574+SverxUtinka@users.noreply.github.com>
2021-02-27 17:33:38 +0200

    Add files via upload

---
+++ articles/script/script.js
@@ -0,0 +1,18 @@
+let start = Date.now();
+
+let timer = setInterval(function() {
+  let timePassed = Date.now() - start;
+
+  if (timePassed >= 900) {
+    clearInterval(timer);
+    return;
+  }
+
+  draw(timePassed);
+
+}, 100);
+
+function draw(timePassed) {
+	var train = document.getElementById("band");
+	train.style.left = timePassed / 1677 + 'px';
+}
